a385798570c79ebf307376ef2b7c4c148f66653c
xinchiqiu <52323991+xinchiqiu@users.noreply.github.com>
2021-01-25 00:28:50 +0000

    update

--- speech_command/normal_training.py
+++ speech_command/normal_training.py
@@ -100,7 +100,7 @@ class RNN(nn.Module):
 # set up model, in_channel = 1 for others, in_channel = n_mels for LSTM
 #model = models.create_model(model_name="LSTM", num_classes=len(CLASSES), in_channels=n_mels)
 
-model = RNN(input_size=n_mels,hidden_size = 64, num_layers = 3, num_classes=len(CLASSES),device= device)
+model = RNN(input_size=n_mels,hidden_size = 256, num_layers = 3, num_classes=len(CLASSES),device= device)
 model.to(device)
 
 
