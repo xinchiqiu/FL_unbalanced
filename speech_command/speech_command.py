f71403c0dd66b828dafe810a03f3dc4edab59dd8
xinchiqiu <52323991+xinchiqiu@users.noreply.github.com>
2021-01-25 15:54:00 +0000

    update

--- speech_command/speech_command.py
+++ speech_command/speech_command.py
@@ -76,32 +76,34 @@ def load_testset():
 # LSTM model:https://github.com/felixchenfy/Speech-Commands-Classification-by-LSTM-PyTorch
 # https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/02-intermediate/recurrent_neural_network/main.py
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, device):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        #self.embeddings = nn.Embedding(input_size,embedding_dim,padding_idx = 0)
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
-        
+        self.device = device
 
     def forward(self, x):
+        #x = self.embeddings(x)
+
         # Set initial hidden and cell states
         batch_size = x.size(0)
-        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).requires_grad_()
-        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).requires_grad_()
+        h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
         
         # Forward propagate LSTM
-        out, _ = self.lstm(x, (h0.detach(), c0.detach())) # shape = (batch_size, seq_length, hidden_size)
+        out, _ = self.lstm(x, (h0.detach(), c0.detach()))  # shape = (batch_size, seq_length, hidden_size)
         
         # Decode the hidden state of the last time step
         out = self.fc(out[:, -1, :])
         return out
 
 
-
 def load_model():
     #model = models.create_model(model_name=models.available_models[0], num_classes=len(CLASSES), in_channels=n_mels)
-    model = RNN(input_size=n_mels,hidden_size = 256, num_layers = 3, num_classes=len(CLASSES))
+    model = RNN(input_size=n_mels,hidden_size = 256, num_layers = 3, num_classes=len(CLASSES),device = device)
     return model
 
 def train(
@@ -112,8 +114,8 @@ def train(
 ) -> None:
     #global global_step
 
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
-    #optimizer = torch.optim.Adam(net.parameters(), lr=0.01, weight_decay=1e-2)
+    #optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001, weight_decay=1e-2)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1, last_epoch=-1)
     criterion = torch.nn.CrossEntropyLoss()
 
